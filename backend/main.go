90a8469ed8fb750c71e8d5fb1d20271c11b0ff52
Dale Short <short.dale@gmail.com>
2024-02-24 15:28:45 -0500

    test button

--- backend/main.go
+++ backend/main.go
@@ -21,16 +21,16 @@ func main() {
 		log.Error(fmt.Sprintf("error opening pin %v", err))
 	}
 
-	// pin :=rpio.Pin(13)  //BCM pin 13 is pin 33 on the actual physical pin headers
-	// pin.Input()
-	// pin.PullDown()
+	pin :=rpio.Pin(6)  //BCM pin 13 is pin 33 on the actual physical pin headers
+	pin.Input()
+	pin.PullDown()
 
 	//500-2500μsec pulse = .0005 seconds to .0025 = 50 cycles to 250 cycles
 	//50hz cycle = .02 seconds per cycle = 2000 clock cycles
 	//100000 hz clock =  .00001 second per cycle
 
 	///home/dale/go/pkg/mod
-	
+
 	pwm1 := rpio.Pin(13)
 	rpio.PinMode(pwm1,rpio.Pwm)
 	rpio.SetDutyCycleWithPwmMode(pwm1,150,2000,true)
@@ -42,9 +42,9 @@ func main() {
 		
 	
 
-		// res := pin.Read()
+		res := pin.Read()
 
-		// log.Debug(fmt.Sprintf("pin is %v", res))
+		log.Debug(fmt.Sprintf("pin is %v", res))
 		rpio.StopPwm()
 		rpio.SetDutyCycleWithPwmMode(pwm1,150,2000,true)
 		pwm1.DutyCycle(150, 2000)
