4f4a9cc10b53178e18330798a3e24ea43f65fe33
Dale Short <short.dale@gmail.com>
2024-02-24 14:15:55 -0500

    add pin test

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/stianeikeland/go-rpio/v4"
 	config "mechied.com/servocoolant/config"
 	slog "mechied.com/servocoolant/logger"
 )
@@ -13,5 +15,22 @@ func main() {
 	config := config.GetConfig(log)
 	fmt.Println(config.GetVersion())
 
+	err := rpio.Open()
 
+	if err != nil {
+		log.Error(fmt.Sprintf("error opening pin %v", err))
+	}
+
+	pin :=rpio.Pin(13)
+	pin.Input()
+	pin.PullDown()
+
+	for {
+
+		res := pin.Read()
+
+		log.Debug(fmt.Sprintf("pin is %v", res))
+		time.Sleep(time.Millisecond*1000)
+	}
+	defer rpio.Close()
 }
