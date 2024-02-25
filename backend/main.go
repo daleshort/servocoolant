d4a3163686ad2b50596f74a8ddde598840a32291
Dale Short <short.dale@gmail.com>
2024-02-24 21:32:49 -0500

    test

--- backend/main.go
+++ backend/main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-
+	"fmt"
 	"net/http"
+
+	log "github.com/sirupsen/logrus"
 	config "mechied.com/servocoolant/config"
 	devicemanager "mechied.com/servocoolant/devicemanager"
 	slog "mechied.com/servocoolant/logger"
-	log "github.com/sirupsen/logrus"
 )
 
-type ServoCoolant struct{
-	log      *log.Logger
-	config   *config.Config
+type ServoCoolant struct {
+	log           *log.Logger
+	config        *config.Config
 	deviceManager *devicemanager.DeviceManager
 }
 
 func main() {
 	sc := ServoCoolant{}
 
-	sc.log= slog.GetLog()
+	sc.log = slog.GetLog()
 	log.Info("Initializing Servo Coolant Nozzle Application ")
 	sc.config = config.GetConfig(sc.log)
 
@@ -28,11 +29,18 @@ func main() {
 
 }
 
-
-
-func (sc *ServoCoolant) Run (){
+func (sc *ServoCoolant) Run() {
 	sc.RegisterEndpoints()
-	
-    log.Fatal(http.ListenAndServe(":8080", nil))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 	//sc.deviceManager.RunAngleTest()
+}
+
+func (sc *ServoCoolant) RegisterEndpoints() {
+	http.HandleFunc("/", sc.handler)
+
+}
+
+func (sc *ServoCoolant) handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
--- backend/router.go
+++ backend/router.go
@@ -1,15 +1,6 @@
-package main
+// package main
 
-import (
-	"net/http"
-	"fmt"
-)
-
-func (sc *ServoCoolant) RegisterEndpoints() {
-	http.HandleFunc("/", sc.handler)
-	
-}
-
-func (sc *ServoCoolant) handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
+// import (
+// 	"net/http"
+// 	"fmt"
+// )
