8e97fb50f807826e5bb9041ca3c4489033272b10
Dale Short <short.dale@gmail.com>
2024-02-24 21:36:17 -0500

    test

--- backend/main.go
+++ backend/main.go
@@ -1,20 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	log "github.com/sirupsen/logrus"
 	config "mechied.com/servocoolant/config"
 	devicemanager "mechied.com/servocoolant/devicemanager"
 	slog "mechied.com/servocoolant/logger"
 )
-
-type ServoCoolant struct {
-	log           *log.Logger
-	config        *config.Config
-	deviceManager *devicemanager.DeviceManager
-}
 
 func main() {
 	sc := ServoCoolant{}
@@ -27,20 +18,4 @@ func main() {
 
 	sc.Run()
 
-}
-
-func (sc *ServoCoolant) Run() {
-	sc.RegisterEndpoints()
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
-	//sc.deviceManager.RunAngleTest()
-}
-
-func (sc *ServoCoolant) RegisterEndpoints() {
-	http.HandleFunc("/", sc.handler)
-
-}
-
-func (sc *ServoCoolant) handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
--- backend/router.go
+++ backend/router.go
@@ -1,6 +1,32 @@
-// package main
+package main
 
-// import (
-// 	"net/http"
-// 	"fmt"
-// )
+import (
+	"fmt"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+	config "mechied.com/servocoolant/config"
+	devicemanager "mechied.com/servocoolant/devicemanager"
+)
+
+type ServoCoolant struct {
+	log           *log.Logger
+	config        *config.Config
+	deviceManager *devicemanager.DeviceManager
+}
+
+func (sc *ServoCoolant) Run() {
+	sc.RegisterEndpoints()
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	//sc.deviceManager.RunAngleTest()
+}
+
+func (sc *ServoCoolant) RegisterEndpoints() {
+	http.HandleFunc("/", sc.handler)
+
+}
+
+func (sc *ServoCoolant) handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
