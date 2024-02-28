e20f2770473aa4697ec8c61ed6651791c1e8d03d
Dale Short <short.dale@gmail.com>
2024-02-27 22:05:50 -0500

    test

--- backend/webmain.go
+++ backend/webmain.go
@@ -8,13 +8,13 @@ import (
 )
 
 type StatusResponse struct {
-	ServoStatus              map[int]ServoDetailStatusResponse `json:"servostatus" example:"100"`
-	IsToolsenseHigh          bool                              `json:"istoolsensehigh" example:"true" `
-	Tools                    map[int]config.Tool               `json:"tools" example:"{1:{length:12.2}}" `
-	ToolQueue                []int                             `json:"toolqueue" example:"[2,1,12]" `
-	IsProgramRunning         bool                              `json:"isprogramrunning" example:"true" `
-	CurrentToolQueuePosition int                               `json:"currenttoolqueueposition" example:"1" `
-
+	ServoStatus     map[int]ServoDetailStatusResponse `json:"servostatus" example:"100"`
+	IsToolsenseHigh bool                              `json:"istoolsensehigh" example:"true" `
+	Tools           map[int]config.Tool               `json:"tools" example:"{1:{length:12.2}}" `
+	ToolQueue       []int                             `json:"toolqueue,omitempty" example:"[2,1,12]" `
+	ToolQueueLength int                               `json:"toolqueuelength"  example:"10" `
+	IsProgramRunning         bool `json:"isprogramrunning" example:"true" `
+	CurrentToolQueuePosition int  `json:"currenttoolqueueposition" example:"1" `
 }
 
 func (sc *ServoCoolant) handlerStatus(w http.ResponseWriter, r *http.Request) {
@@ -23,12 +23,14 @@ func (sc *ServoCoolant) handlerStatus(w http.ResponseWriter, r *http.Request) {
 
 		tools, _ := sc.config.GetAllToolLengths()
 		resp := StatusResponse{
-			ServoStatus:     sc.getServoStatus(),
-			IsToolsenseHigh: sc.deviceManager.IsToolsenseHigh,
-			Tools:           tools,
-			ToolQueue:       sc.autoManager.ToolQueue,
-			IsProgramRunning: sc.autoManager.IsProgramRunning,
+			ServoStatus:              sc.getServoStatus(),
+			IsToolsenseHigh:          sc.deviceManager.IsToolsenseHigh,
+			Tools:                    tools,
+			ToolQueue:                sc.autoManager.ToolQueue,
+			ToolQueueLength:         len(sc.autoManager.ToolQueue),
+			IsProgramRunning:         sc.autoManager.IsProgramRunning,
 			CurrentToolQueuePosition: sc.autoManager.CurrentToolQueuePosition,
+
 		}
 
 		w.Header().Set("Content-Type", "application/json")
